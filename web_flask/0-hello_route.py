4d342ca42e7a2a075e0c1bb281c65b96d701d594
Ubuntu <korir.chelel@gmail.com>
2024-03-15 08:54:03 +0000

    spelling mistakes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
     """ Returns some text. """
-    return 'Hello HBNB'
+    return 'Hello HBNB!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=None)
